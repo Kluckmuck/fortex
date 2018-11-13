38d5413e2a8eb7c475cb6ca5b57a59c9b1a30b0b
Kluckmuck <kluckmucki@gmail.com>
2018-11-13 18:57:04 +0100

    very simple navbar with angular material

--- fortex-front/src/app/app.module.ts
+++ fortex-front/src/app/app.module.ts
@@ -7,18 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 //Components
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 //Material
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +30,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule,
     FormsModule
   ],
   providers: [],
